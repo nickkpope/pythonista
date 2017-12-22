a4d1bc5698448f82c20de8a3f85033703e93f87b
nick <nickkpope@gmail.com>
2017-12-22 01:47:18 +0000

    plotting daily and hourly usage and plot controlling for the external temperature

--- hydro_viz/hydro_quebec.py
+++ hydro_viz/hydro_quebec.py
@@ -1,6 +1,19 @@
 import csv
 import appex
-from datetime import datetime
+
+import collections
+import datetime
+
+
+def control_for_hourly_temp(kwh, temp):
+	normalized_temp = (temp - min_hourly_temp)/(max_hourly_temp - min_hourly_temp)
+	return kwh * normalized_temp
+
+
+def control_for_daily_temp(kwh, temp):
+	normalized_temp = (temp - min_daily_temp)/(max_daily_temp - min_daily_temp)
+	return kwh * normalized_temp
+
 
 def to_cad(kwh):
 	''' Converts from Kilowatt hours to Candian dollars. 
@@ -8,20 +21,84 @@ def to_cad(kwh):
 	return kwh*252.56/2829
 
 
-def kwh_per_day():
+def csv_reader(filename):
 	if appex.is_running_extension():
 		file_path = appex.get_text()
 	else:
-		file_path = 'raw_data.csv'
+		file_path = filename
 		
 	stream = open(file_path, 'rb').read().decode('utf-8').split('\n')
-	reader = iter(csv.reader(stream, delimiter=';'))
+	return iter(csv.reader(stream, delimiter=';'))
 	
+
+def kwh_by_day():
+	
+	reader = csv_reader('daily_usage.csv')
 	headers = reader.__next__()
 	data = []
 	
 	for line in reader:
 		if not line:
 			continue
-		yield(datetime.strptime(line[2], '%Y-%m-%d'), float(line[3]))
+		kwh = float(line[3]) if line[3] else 0.0
+		yield datetime.datetime.strptime(line[2], '%Y-%m-%d'), kwh, control_for_daily_temp(kwh, int(line[5]))
 
+
+def temp_by_day():
+	reader = csv_reader('daily_usage.csv')
+	headers = reader.__next__()
+	
+	for line in reader:
+		if not line:
+			continue
+
+		yield int(line[5])
+
+
+def temp_by_hour():
+	reader = csv_reader('hourly_usage.csv')
+	headers = reader.__next__()
+	
+	for line in reader:
+		if not line:
+			continue
+
+		yield int(line[4])
+
+def kwh_by_hour():
+	reader = csv_reader('hourly_usage.csv')
+	headers = reader.__next__()
+	
+	for line in reader:
+		if not line:
+			continue
+		
+		kwh = float(line[2]) if line[2] else 0.0
+		# 2017-12-18 20:00:00
+		yield datetime.datetime.strptime(line[1], '%Y-%m-%d %H:%M:%S'), control_for_hourly_temp(kwh, int(line[4]))
+
+
+def avg_kwh_by_hour_of_day():
+	bucket = collections.defaultdict(
+		lambda: collections.defaultdict(list))
+	for time_of_day, kwh in kwh_by_hour():
+		td = time_of_day.hour+\
+			time_of_day.minute/60+\
+			time_of_day.second/120
+		day_of_the_week = time_of_day.weekday()
+		bucket[day_of_the_week][td].append(kwh)
+
+	ret = collections.defaultdict(dict)
+	for dofw, ds in bucket.items():
+		for td, kwhs in ds.items():
+			ret[dofw][td] = float(sum(kwhs))/len(kwhs)
+
+	return ret
+
+hourly_temps = list(temp_by_hour())
+min_hourly_temp = min(hourly_temps)
+max_hourly_temp = max(hourly_temps)
+
+daily_temps = list(temp_by_day())
+min_daily_temp = min(daily_temps)
+max_daily_temp = max(daily_temps)
--- hydro_viz/plotting.py
+++ hydro_viz/plotting.py
@@ -1,21 +1,67 @@
 import matplotlib.pyplot as plt
+import console
 
 import hydro_quebec
 
+dofw_labels = [
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+	'Sunday',
+]
 
-dates, kwhs = zip(*hydro_quebec.kwh_per_day())
 
-plt.subplot(2, 1, 1)
-plt.plot(dates, kwhs)
-plt.title('Kilowatts per day.')
-#plt.xlabel('Day')
-plt.ylabel('Energy (kwh)')
+def plot_kwhs_per_day():
 
-plt.subplot(2, 1, 2)
-plt.plot(dates, [hydro_quebec.to_cad(item) for item in kwhs], 'r.-')
-plt.title('Cost per day.')
-plt.xlabel('Day')
-plt.ylabel('Cost (CAD)')
+	dates, kwhs, ckwhs = zip(*hydro_quebec.kwh_by_day())
 
-plt.show()
+	ax = plt.subplot(2, 1, 1)
+	plt.plot(dates, kwhs, label='kwh')
+	plt.plot(dates, ckwhs, label='controlled')
+	
+	plt.title('Kilowatts per day.')
+	#plt.xlabel('Day')
+	plt.ylabel('Usage')
+	ax.legend(bbox_to_anchor=(0.3, 1.2))
+
+	plt.subplot(2, 1, 2)
+	plt.plot(dates, [hydro_quebec.to_cad(item) for item in kwhs], 'r.-')
+	plt.title('Cost per day.')
+	plt.xlabel('Day')
+	plt.ylabel('Cost (CAD)')
+	
+	plt.show()
+	
+	
+def plot_daily_usage():
+	import matplotlib.mlab as mlab
+	import matplotlib.pyplot as plt
+	
+	# need to sum the kwhs for each hour of the day
+	for dofw, adu in hydro_quebec.avg_kwh_by_hour_of_day().items():
+		x, y = zip(*adu.items())
+
+		ret=plt.plot(x, y, label=dofw_labels[dofw])
+
+	plt.legend(bbox_to_anchor=(1.2, 1))
+	plt.xlabel('Time of Day')
+	plt.ylabel('Usage (controlled)')
+	plt.title('Usage throughout the day.')
+	
+	plt.show()
+
+plot_map = {
+	1: plot_kwhs_per_day,
+	2: plot_daily_usage,
+}
+plot_map[console.alert(
+	'Views',
+	'Choise which plot you would like to see.',
+	'Kwh per day',
+	'Daily usage',
+)]()
+	
 
